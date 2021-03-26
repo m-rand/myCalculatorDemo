8b506bc726b27010f2b830e9e9e25bc6f331068d
Marcel Balas <marcel.balas@gmail.com>
2021-03-26 17:15:56 +0100

    Initial Commit

---
+++ Shared/myCalculatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCalculatorDemoApp.swift
+//  Shared
+//
+//  Created by Marcel Baláš on 26.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct myCalculatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
